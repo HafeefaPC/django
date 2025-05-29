c3e06e57ad7ff1bd8e40cd442e44f95fbcf5f46e
HafeefaPC <hafeefapc2003@gmail.com>
2025-05-29 20:49:42 +0530

    feat: staticfiles like images,styles,javascript styles are imported in the django project

---
+++ lesson-4staticfile/staticfile/djangoapp/static/js/script.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Page is Loaded and Script is running successfully!');
+    alert('Welcome to the Static File Example!');});
